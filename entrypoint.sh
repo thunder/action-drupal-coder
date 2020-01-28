8b2a2411be3f75d40862ad0b4324fd79069d53a6
Christian Fritsch <chr.fritsch@gmx.net>
2020-01-28 14:54:09 +0100

    Foo

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,8 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 phpcs --config-set installed_paths ~/.composer/vendor/drupal/coder/coder_sniffer
 
+phpcs --standard=Drupal --extensions='php,module,inc,install,test,profile,theme' --report=checkstyle -q .
+
 phpcs --standard=Drupal --extensions='php,module,inc,install,test,profile,theme' --report=checkstyle -q . \
   | reviewdog -f="checkstyle" -name="drupal-coder (drupal)" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
 
